9fdbd80df2730ab5c33200726ad86165e30f749f
leehoa <dvvu@tma.com.vn>
2016-12-19 09:19:46 +0700

    Initial Commit

---
+++ UITCE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITCE
+//
+//  Created by Lee Hoa on 12/19/16.
+//  Copyright © 2016 Lee Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
