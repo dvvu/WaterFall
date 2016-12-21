0ad71f57d6607cce3dbad09527518674ea70db48
leehoa <dvvu@tma.com.vn>
2016-12-21 16:41:22 +0700

    [MOBILE-2012] setting page.

---
+++ UITCE/ClockViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClockViewController.swift
+//  UITCE
+//
+//  Created by Lee Hoa on 12/21/16.
+//  Copyright © 2016 Lee Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
